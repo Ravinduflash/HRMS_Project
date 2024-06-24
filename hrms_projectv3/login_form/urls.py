ed934d0992b52420d56c8a1b5114e44e2d733ddf
Ravindu <dulshanravindu505@gmail.com>
2024-06-24 23:38:29 +0530

    "Updated templates for employee and HR manager dashboards, login and signup forms, and added password reset views and URLs."

--- hrms_projectv3/login_form/urls.py
+++ hrms_projectv3/login_form/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import login_view, signup_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
