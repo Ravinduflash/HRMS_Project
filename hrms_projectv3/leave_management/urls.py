873c83dc743b3dd89bf3bf102664f22f0f2c8ed6
Ravindu <dulshanravindu505@gmail.com>
2024-06-24 13:38:27 +0530

    "Updated db.sqlite3 and modified urls.py in leave_management to rename hr_manager_leave_management view to hr_leave_management"

--- hrms_projectv3/leave_management/urls.py
+++ hrms_projectv3/leave_management/urls.py
@@ -1,11 +1,9 @@
-# urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('apply_leave_request/', views.apply_leave_request, name='apply_leave_request'),
     path('employee_leave_page/', views.employee_leave_page, name='employee_leave_page'),
-    path('hr_manager_leave_management/', views.hr_manager_leave_management, name='hr_manager_leave_management'),
+    path('hr_manager_leave_management/', views.hr_leave_management, name='hr_manager_leave_management'),
     # Add other URLs as needed
 ]
